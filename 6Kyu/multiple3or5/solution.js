beec90f28615bc827d6492a2f456708edc2b395c
MatteoR1994 <matteo.rinaldi1893@gmail.com>
2022-02-16 09:34:19 +0100

    Update solution.js

--- 6Kyu/multiple3or5/solution.js
+++ 6Kyu/multiple3or5/solution.js
@@ -1,19 +1,16 @@
 function solution(number) {
-    if (number < 0) {
-        return 0;
-    }
-    let multiples = [];
-    for (let i = 0; i < number; i++) {
-        if (i % 3 === 0 || i % 5 === 0) {
-            multiples.push(i);
-        }      
-    }
-    return multiples.reduce((p, c) => p + c, 0);
+    // if (number < 0) {
+    //     return 0;
+    // }
+    // let multiples = [];
+    // for (let i = 0; i < number; i++) {
+    //     if (i % 3 === 0 || i % 5 === 0) {
+    //         multiples.push(i);
+    //     }      
+    // }
+    // return multiples.reduce((p, c) => p + c, 0);
 
-    // let range = [...new Array(number).keys()];
-    // return number < 0 ? 0 : (range.reduce((p, c) => c % 3 === 0 || c % 5 === 0 ? p + c : p, 0));
-
-    // return number < 0 ? 0 : ([...new Array(number).keys()].reduce((p, c) => c % 3 === 0 || c % 5 === 0 ? p + c : p, 0));
+    return number < 0 ? 0 : ([...new Array(number).keys()].reduce((p, c) => c % 3 === 0 || c % 5 === 0 ? p + c : p, 0));
 }
 
 console.log("Con -3 mi aspetto 0: ", solution(-3));
