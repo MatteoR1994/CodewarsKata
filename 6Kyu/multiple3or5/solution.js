34bf5c0e354cba221b628ae378d56e36b2a1edba
MatteoR1994 <matteo.rinaldi1893@gmail.com>
2022-02-16 09:56:51 +0100

    Update solution.js

--- 6Kyu/multiple3or5/solution.js
+++ 6Kyu/multiple3or5/solution.js
@@ -11,6 +11,7 @@ function solution(number) {
     // return multiples.reduce((p, c) => p + c, 0);
 
     return number < 0 ? 0 : ([...new Array(number).keys()].reduce((p, c) => c % 3 === 0 || c % 5 === 0 ? p + c : p, 0));
+    // return [...new Array(number < 0 ? 0 : number).keys()].reduce((p, c) => c % 3 === 0 || c % 5 === 0 ? p + c : p, 0);
 }
 
 console.log("Con -3 mi aspetto 0: ", solution(-3));
